b4c12dd815f59f3ceb9e51143f072e8c93bb06f6
Soumik Rakshit <soumikrakshit@yandex.com>
2016-04-24 09:06:21 +0530

    mmm

--- note.py
+++ note.py
@@ -12,7 +12,9 @@ with open(filename, 'r') as f:
     notes = f.read().strip().split('\n')
     a = argparse.ArgumentParser()
     a.add_argument('-r', '--remove', metavar='NOTE_NUMBER', type=int, default=0xffffffff, help='The note to remove')
-    a.add_argument('-a', '--add', metavar='NOTE', type=str, default='', help='The note to add')
+    a.add_argument('-a', '--append', metavar='NOTE', type=str, default='', help='The note to append')
+    a.add_argument('-p', '--prepend', metavar='NOTE', type=str, default='', help='The note to prepend')
+
     a = a.parse_args()
     if a.remove != 0xffffffff:
         r = a.remove
@@ -20,8 +22,12 @@ with open(filename, 'r') as f:
             r = len(notes) + r
         notes = [notes[i] for i in range(0, r)] + [notes[i] for i in range(r + 1, len(notes))]
         modified = True
-    if a.add != '':
-        notes.append(a.add)
+    if a.append != '':
+        notes.append(a.append)
+        modified = True
+
+    if a.prepend != '':
+        notes = [a.prepend] + notes
         modified = True
 
 if modified:
