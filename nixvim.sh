6476a90eb5ba64e82056d9f7852487085693a3a4
Soumik Rakshit <soumikrakshit@yandex.com>
2016-06-04 21:39:32 +0530

    nm

--- nixvim.sh
+++ nixvim.sh
@@ -6,4 +6,4 @@
 # On NixOS due to the lack of libtinfo.so I have created a symlink at
 # ~/.local/lib. So LD_LIBRARY_PATH that
 
-LD_LIBRARY_PATH=$HOME/.local/lib nvim
+LD_LIBRARY_PATH=$HOME/.local/lib nvim $@
