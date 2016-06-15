99627c25558f0ce4ed60a30acb9985086c97db1d
Soumik Rakshit <soumikrakshit@yandex.com>
2016-06-15 20:04:05 +0530

    nm

--- nixvim.sh
+++ nixvim.sh
@@ -6,4 +6,11 @@
 # On NixOS due to the lack of libtinfo.so I have created a symlink at
 # ~/.local/lib. So LD_LIBRARY_PATH that
 
+thepath=$HOME/.local/lib/libtinfo.so.5
+
+
+if [ ! -e "$thepath" ] ; then
+  echo "$thepath does not point to the required shared lib!! Fix that"
+fi
+
 LD_LIBRARY_PATH=$HOME/.local/lib nvim $@
