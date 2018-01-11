24a4fba0dfe04176d8ea498067767bca92d69b04
Soumik Rakshit <soumikrakshit@yandex.com>
2018-01-11 10:58:39 +0530

    bm

--- build.py
+++ build.py
@@ -5,9 +5,14 @@ import os
 import argparse
 
 # Put files here
-files = ['weekend.cpp']
+files = ['weekend.cpp', 'rng.cpp']
 
-command_line_cflags = []
+# Default path to compiler
+cc = 'g++'
+
+cwd = os.getcwd()
+
+command_line_cflags = ["-L\"{}\"".format(cwd), '-lSDL2main', '-lSDL2']
 
 def pkgconfig(libname, extra_args):
     out = sp.Popen(['pkg-config', libname] + extra_args, stdout=sp.PIPE)
@@ -20,12 +25,13 @@ def pkgconfig(libname, extra_args):
 
 def run_gcc():
     # Call pkgconfig or put libflags here (as a list)
-    sdl_flags = pkgconfig('sdl2', ['--libs', '--cflags'])
+    # sdl_flags = pkgconfig('sdl2', ['--libs', '--cflags'])
+    sdl_flags = []
 
     include_path = '-I' + str(os.path.dirname(os.path.abspath(__file__)))
 
     # Append lists here
-    all_flags = ['g++', '-std=c++17', include_path] + command_line_cflags  + sdl_flags + files
+    all_flags = [cc, include_path] + sdl_flags + files + command_line_cflags
     print('Build line = ', ' '.join(all_flags))
     sp.run(all_flags, check=True)
 
@@ -34,13 +40,21 @@ if __name__ == '__main__':
     a = argparse.ArgumentParser(usage='Shove .cpp files to gcc')
 
     # Example - ./build.py ,-ggdb,-Wall,-Wextra
-    a.add_argument('cflags', default='', help="sequence of ',' and option. Options sent to gcc.")
+    a.add_argument('cflags', nargs='?', default='', help="sequence of ',' and option. Options sent to gcc.")
+    a.add_argument('-f', '--files', nargs='?', default='', help="Override the list of files. Sequence of ',' and filename")
+    a.add_argument('-c', '--cc', nargs='?', default='g++', help="Override path to compiler")
 
     args = a.parse_args()
 
     for opt in args.cflags.split(','):
         if opt != '':
             command_line_cflags.append(opt)
+
+    if args.files != '':
+        files = args.files.split(',')[1:]
+
+    if args.cc != 'g++':
+        cc = args.cc
 
     print(command_line_cflags)
 
