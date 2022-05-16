41fb0fc607f12ea7d503a88616a435d3ffeda5b2
Soumik <soumik.rakshit@go-jek.com>
2022-05-17 03:07:51 +0530

    namesubst update

--- bin/namesubst.py
+++ bin/namesubst.py
@@ -27,6 +27,7 @@ class Config:
         self.changed_paths = []
         self.do_change_paths = False
         self.do_subst_content = True
+        self.ignore_substring = False
 
 config = Config()
 
@@ -35,6 +36,10 @@ def rename_file(dirpath, filename, mode):
 
     old_filepath = os.path.join(dirpath, filename)
     new_filepath = os.path.join(dirpath, new_filename)
+
+    if config.ignore_substring in old_filepath:
+        print(f'Ignoring path {old_filepath}')
+        return
 
     if new_filename != filename:
         print(f'Renamed {mode} {old_filepath} to {new_filepath}')
@@ -103,6 +108,10 @@ def walk_directory(d):
 
 
 def replace_in_file(filepath):
+    if config.ignore_substring in filepath:
+        print(f'Ignoring contents in {filepath}')
+        return
+
     # print('Replacing in file', filepath)
     with open(filepath, 'r') as stream:
         source = stream.read()
@@ -136,8 +145,8 @@ if __name__ == '__main__':
     ap.add_argument('-n', '--newstring', default=None, help='new string')
     ap.add_argument('-y', '--yes', action='store_true', help='Yes, do actually modify the files instead of a dry run')
     ap.add_argument('-p', '--rename-files', action='store_true', default=False, help='change directory and file names also')
-
     ap.add_argument('-o', '--only-rename-files', action='store_true', default=False, help='change directory and file names only, not their contents')
+    ap.add_argument('-i', '--ignore-substring', default='', help='ignore path if it contains given substring')
 
     args = ap.parse_args()
     print('Traversing directories - ' + str(args.directories))
@@ -153,6 +162,8 @@ if __name__ == '__main__':
 
     if args.only_rename_files:
         config.do_subst_content = False
+
+    config.ignore_substring = args.ignore_substring
 
     config.is_dry_run = not args.yes
     if config.is_dry_run:
