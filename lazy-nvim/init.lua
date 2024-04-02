17f2e569ad8b2ba1580e082bf8718e27c3faec52
Soumik Rakshit <soumik@sostronk.com>
2024-04-02 22:54:23 +0530

    Add lazy vim config

---
+++ lazy-nvim/init.lua
@@ -0,0 +1,9 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+if vim.g.neovide then
+  vim.o.guifont = "NanumGothicCoding:h16"
+  vim.g.neovide_cursor_trail_size = 0.2
+  vim.g.neovide_animation_length = 0.05
+  vim.g.neovide_scroll_animation_far_lines = 0
+end
