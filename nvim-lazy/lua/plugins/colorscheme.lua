b3b61cfcd5eed63a0222137491270f26d8d8b0bb
Soumik Rakshit <soumik@sostronk.com>
2024-05-18 20:43:50 +0530

    Add lazyvim config

---
+++ nvim-lazy/lua/plugins/colorscheme.lua
@@ -0,0 +1,23 @@
+return {
+  { "rebelot/kanagawa.nvim" },
+
+  {
+    "olivercederborg/poimandres.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("poimandres").setup({
+        -- leave this setup function empty for default config
+        -- or refer to the configuration section
+        -- for configuration options
+      })
+    end,
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa",
+    },
+  },
+}
